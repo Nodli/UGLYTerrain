28feb07025af7d37c021feb3acc0fe34bc5eee2c
Francois Crevits <francois.crevits@hotmail.fr>
2018-11-14 11:57:11 +0100

    test erode and create

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,26 @@
 int main()
 {
 	MultiLayerMap mlm(100, 100, { -25, -25}, {25, 25});
-	ScalarField &sf = mlm.new_field();
-	
+	mlm.new_field();
+	ScalarField &sediment = mlm.new_field();
+	ScalarField &bedrock = mlm.get_field(0);
+
 	TerrainNoise t_noise(10.0, 1.0 / 200.0, 8);
 
 	for(int j = 0; j < 100; ++j)
 	{
 		for(int i = 0; i < 100; i++)
 		{
-			sf.at(i, j) = t_noise.getNoise(i, j);
+			bedrock.at(i, j) = t_noise.getNoise(i, j);
+			sediment.at(i, j) = bedrock.get_value(i, j) + 1.0;
 		}
 	}
 
-
-
-	sf.exportAsObj("Terrain.obj");
-
-	mlm.erode(0.5);
-	sf.exportAsObj("TerrainErode.obj");
+	ScalarField fusion = mlm.generate_field();
+	fusion.exportAsObj("BedrockAndSediment.obj");
+	mlm.erode_and_create(0.5);
+	fusion = mlm.generate_field();
+	fusion.exportAsObj("BedrockAndSedimentEroded.obj");
 
 	return 0;
 }
