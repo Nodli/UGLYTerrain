c7b79ff5213ebb986b70fd3e9a900a2fafc76161
Nodli <rambure.hugo@hotmail.fr>
2018-11-21 11:39:23 +0100

    Adding Utils

---
+++ include/Utils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * @brief Computes the proportions of each value
+ *
+ * @param n         the number of values
+ * @param values	the values to compute the proportions
+ */
+void proportion(int n, double* values, double* proportions);
---
+++ src/Utils.cpp
@@ -0,0 +1,14 @@
+#include <Utils.hpp>
+
+void proportion(int n, double* values, double* proportions){
+
+	double sum = 0;
+	for(int ivalue = 0; ivalue != n; ++ivalue){
+		sum += values[ivalue];
+	}
+
+	for(int ivalue = 0; ivalue != n; ++ivalue){
+		proportions[ivalue] = values[ivalue] / sum;
+	}
+
+}
