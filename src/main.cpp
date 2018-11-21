15f09df19ca0997ccca15b2c2567ae7e84bbbaef
Francois Crevits <francois.crevits@hotmail.fr>
2018-11-21 13:20:02 +0100

    compiling

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main()
 	erode_and_transport(mlm, 0.1);
 	mlm.get_field(0).export_as_pgm("TerrainErode.pgm", true);
 	mlm.get_field(0).export_as_obj("TerrainErode.obj");
-	mlm.get_field(1).export_as_obj("TerrainEroded.obj");
+	// mlm.get_field(1).export_as_obj("TerrainEroded.obj");
 
 	ScalarField area = get_area(mlm);
 	area.export_as_pgm("HydraulicArea.pgm", true);
