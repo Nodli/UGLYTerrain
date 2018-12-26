8db3048fe4dff99bd12ab0e2a9a9c8dfbfae115c
p1409417 <thomas.bonis@etu.univ-lyon1.fr>
2018-12-26 20:11:32 +0100

    Adding a double field to merge field utilities

---
+++ include/DoubleField.hpp
@@ -0,0 +1,376 @@
+#pragma once
+
+#include <Grid2d.hpp>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+/**
+ * @brief Defines a field of values spread across a grid on the plane
+ *
+ */
+class DoubleField : public Grid2d
+{
+public:
+	class read_only_iterator;
+	/**
+	 * @brief Return a read only iterator to the first element of the field
+	 * 
+	 * @return read_only_iterator 	an iterator to the first element of the field
+	 */
+	read_only_iterator begin() const;
+
+	/**
+	 * @brief Return a read only iterator to the last element of the field
+	 * 
+	 * @return read_only_iterator 	an iterator to the last element of the field
+	 */
+	read_only_iterator end() const;
+
+public:
+	DoubleField() = delete;
+	/**
+	 * @brief Construct a new Field object from an other Field
+	 *
+	 * @param hf        the field to copy
+	 */
+
+	DoubleField(const DoubleField& hf)
+		: Grid2d(hf) {}
+	/**
+	 * @brief Construct a new Field object from an other Field
+	 *
+	 * @param hf        the Field to copy
+	 */
+	DoubleField(DoubleField&& hf)
+		: Grid2d(std::move(hf)) {}
+
+	/**
+	 * @brief Construct a new empty Field object from a grid
+	 *
+	 * @param g         the initial grid of the Field
+	 */
+	DoubleField(const Grid2d &g)
+		: Grid2d(g) {}
+	/**
+	 * @brief Construct a new Field object from a box
+	 *
+	 * @param b         the box in wich to create the grid
+	 * @param width     the number of cells along the width of the grid
+	 * @param height    the number of cells along the height of the grid
+	 */
+	DoubleField(const Box2d &b, const int width, const int height)
+		: Grid2d(b, width, height) {}
+	/**
+	 * @brief Construct a new Field object
+	 *
+	 * @param width     the number of cells along the width of the grid
+	 * @param height    the number of cells along the height of the grid
+	 * @param a         the first point of the grid
+	 * @param b         the second point of the grid
+	 */
+	DoubleField(const int width, const int height, const Eigen::Vector2d a = {0, 0}, const Eigen::Vector2d b = {1, 1})
+		: Grid2d(width, height, a, b) {}
+
+	/**
+	 * @brief Get the value of the field at a given cell
+	 *
+	 * @param i, j      the position of the cell on the grid
+	 * @return double   the exact value of the field in that cell
+	 */
+	virtual double value(const int i, const int j) const = 0;
+
+	/**
+	 * @brief Get the value of the field at a given cell
+	 *
+	 * @param p         the position of the cell on the grid
+	 * @return double   the exact value of the field in that cell
+	 */
+	double value(const Eigen::Vector2i p) const
+	{
+		return value(p(0), p(1));
+	}
+
+	/**
+	 * @brief Get the interpolated value of the field at a given position on the plane
+	 *
+	 * @param x, y      the position of a point on the plane
+	 * @return double   the interpolated value of the field at that position
+	 */
+	double value(const double x, const double y) const;
+
+	/**
+	 * @brief Get the min value of the field
+	 *
+	 * @return double   the min value
+	 */
+	double get_min() const;
+
+	/**
+	 * @brief Get the max value of the field
+	 *
+	 * @return double   the max value
+	 */
+	double get_max() const;
+
+	/**
+	 * @brief Get range of values in field
+	 *
+	 * @return double       the range
+	 */
+	double get_range() const;
+
+	/**
+	 * @brief Get sum of values in field
+	 *
+	 * @return double       the sum of all values
+	 */
+	double get_sum() const;
+	
+	/**
+	 * @brief Calculate the slope of the field at a given cell.
+	 * The slope is calculated to be the norm of the gradient
+	 *
+	 * @param p         the position of the cell on the grid
+	 * @return double   the value of the slope at that cell
+	 */
+	double slope(const Eigen::Vector2i p) const;
+
+	/**
+	 * @brief Calculate the slope of the field at a given cell.
+	 * The slope is calculated to be the norm of the gradient
+	 *
+	 * @param i, j      the position of the cell on the grid
+	 * @return double   the value of the slope at that cell
+	 */
+	double slope(const int i, const int j) const;
+
+	/**
+	 * @brief Calculate the gradient of the field at a given cell
+	 *
+	 * @param i, j      the position of the cell on the grid
+	 * @return Eigen::Vector2d  the gradient at that cell
+	 */
+	Eigen::Vector2d gradient(const int i, const int j) const;
+	/**
+	 * @brief Calculate the normal to the gradient at a given cell
+	 *
+	 * @param i, j      the position of the cell on the grid
+	 * @return Eigen::Vector3d  the normal at that cell
+	 */
+	Eigen::Vector3d normal(const int i, const int j) const;
+
+	/**
+	 * @brief Get indices of values sorted by height
+	 *
+	 * @return vector      the indices of values sorted by height
+	 */
+	std::vector<std::pair<double, Eigen::Vector2i>> sort_by_height() const;
+
+	/**
+	 * @brief Get all the information of a neigborhood
+	 *
+	 * @param pos       the position of the cell on the grid
+	 * @param v         the value of the neighbors
+	 * @param p         the positions of the neighbors
+	 * @param s         the slopes of the neighbors
+	 * @return int      the number of neigbors
+	 */
+	int neighbors_info(const Eigen::Vector2i pos, double v[8], Eigen::Vector2i p[8], double s[8]) const
+	{
+		return neighbors_info(pos(0), pos(1), v, p, s);
+	}
+
+	/**
+	 * @brief Get all the information of a neigborhood
+	 *
+	 * @param i, j      the position of the cell on the grid
+	 * @param v         the value of the neighbors
+	 * @param p         the positions of the neighbors
+	 * @param s         the slopes of the neighbors
+	 * @return int      the number of neigbors
+	 */
+	int neighbors_info(const int i, const int j, double v[8], Eigen::Vector2i p[8], double s[8]) const;
+
+	/**
+	 * @brief Get the information of a neigborhood if the slope is superior to a threshold value
+	 *
+	 * @param pos       the position of the cell on the grid
+	 * @param v         the value of the neighbors
+	 * @param p         the positions of the neighbors
+	 * @param s         the slopes of the neighbors
+	 * @param s_filter  the minimal slope value to be considered as a neighbor
+	 * @param sup       1 if s > s_filter and 0 if s < s_filter using signed values
+	 * @return int      the number of neigbors
+	 */
+	int neighbors_info_filter(const Eigen::Vector2i pos, double v[8], Eigen::Vector2i p[8], double s[8], const double s_filter = 0., const bool sup = false) const
+	{
+		return neighbors_info_filter(pos(0), pos(1), v, p, s);
+	}
+
+	/**
+	 * @brief Get all the information of a neigborhood
+	 *
+	 * @param i, j      the position of the cell on the grid
+	 * @param v         the value of the neighbors
+	 * @param p         the positions of the neighbors
+	 * @param s         the slopes of the neighbors
+	 * @param s_filter  the minimal slope value to be considered as a neighbor
+	 * @return int      the number of neigbors
+	 */
+	int neighbors_info_filter(const int i, const int j, double v[8], Eigen::Vector2i p[8], double s[8], const double s_filter = 0., const bool sup = false) const;
+
+
+	/**
+	 * @brief Affectation operator
+	 *
+	 * @param sf            the Field to affect
+	 * @return DoubleField& a reference to this Field
+	 */
+	DoubleField& operator=(const DoubleField& sf);
+
+	/**
+	 * @brief Affectation operator
+	 *
+	 * @param sf            the field to affect
+	 * @return DoubleField& a reference to this Field
+	 */
+	DoubleField& operator=(DoubleField&& sf);
+
+
+	/**
+	 * @brief Export the Field as a obj
+	 *
+	 * @param filename      the name of the file
+	 * @param name          the name of the object in the file
+	 */
+	void export_as_obj(const std::string filename, std::string name = "") const;
+
+	/**
+	 * @brief Export the Field as a pgm
+	 *
+	 * @param filename      the name of the file
+	 * @param minMan        tells of the values should be ranged automatically
+	 * @param rangeMin      give a minimal value
+	 * @param rangeMax      give a maximal value
+	 */
+	void export_as_pgm(const std::string filename, bool minMan = true, double rangeMin = 0, double rangeMax = 1) const;
+
+	/**
+	 * @brief export the Field as a list of pairs <value/position>
+	 *
+	 * @return std::vector<std::pair<double, Eigen::Vector2i>>  a list of pair <value/cell_position> representing the Scalar Field
+	 */
+	std::vector<std::pair<double, Eigen::Vector2i>> export_to_list() const;
+
+protected:
+	/**
+	 * @brief Gets the index on the array of values for a given position
+	 *
+	 * @param i, j      the position on the grid
+	 * @throw           invalid_argument if the position required is not on the grid
+	 * @return int      the index of that positition in the array of values
+	 */
+	int index(const int i, const int j) const
+	{
+		int index = _grid_width * j + i;
+
+		if(index >= cell_number())
+		{
+			throw std::invalid_argument("wrong access to a value in the Field");
+		}
+
+		return index;
+	}
+
+	/**
+	 * @brief Gets the position of an index
+	 * 
+	 * @param index 			the index of the cell
+	 * @return Eigen::Vector2i 	the position of the cell on the grid
+	 */
+	Eigen::Vector2i posi_from_index(const int index) const
+	{
+		if(index >= cell_number())
+		{
+			throw std::invalid_argument("wrong access to a value in the Field");
+		}
+
+		return Eigen::Vector2i(index%_grid_width, index/_grid_width);
+	}
+};
+
+/**
+ * @brief Class iterator for the double_field.
+ * This only allows the reading of the values.
+ * 
+ */
+class DoubleField::read_only_iterator
+{
+public:
+	/**
+	 * @brief Construct a new read only iterator object
+	 * 
+	 * @param parent 		The parent DoubleField
+	 * @param position 		The position in the doublefield
+	 */
+	read_only_iterator(const DoubleField* parent, const int position) :
+		_parent(parent), _position(position) {}
+	/**
+	 * @brief Copy a read only iterator object
+	 * 
+	 * @param roi 			The iterator to copy		
+	 */
+	read_only_iterator(const read_only_iterator& roi) :
+		_parent(roi._parent), _position(roi._position) {}
+	
+	/**
+	 * @brief Get the value pointed by the iterator
+	 * 
+	 * @return double 		The value pointed by the iterator
+	 */
+	double operator*() const;
+
+	/**
+	 * @brief Equality operator between two iterators
+	 * 
+	 * @param roi 		the iterator to be compared to
+	 * @return true 	if the two iterators are equal
+	 * @return false 	if the two iterators are not equal
+	 */
+	bool operator==(const read_only_iterator& roi) const;
+
+	/**
+	 * @brief Inequality operator
+	 * 
+	 * @param roi 		the iterator to be compared to
+	 * @return true 	if the two iterators are not equal
+	 * @return false 	if the two iterators are equal
+	 */
+	bool operator!=(const read_only_iterator& roi) const;
+
+	/**
+	 * @brief Incremental operator, points to the next value in the field
+	 * 
+	 * @return read_only_iterator& 
+	 */
+	read_only_iterator& operator++();
+	/**
+	 * @brief Incremental operator, points to the next value in the field
+	 * 
+	 * @param i 
+	 * @return read_only_iterator 
+	 */
+	read_only_iterator operator++(int i);
+	/**
+	 * @brief Affectation operator
+	 * 
+	 * @param roi 		The iterator to copy
+	 * @return read_only_iterator& 
+	 */
+	read_only_iterator& operator=(const read_only_iterator& roi);
+
+private:
+	const DoubleField * _parent;
+	int _position;
+};
---
+++ src/DoubleField.cpp
@@ -0,0 +1,324 @@
+#include <DoubleField.hpp>
+#include <algorithm>
+
+
+DoubleField::read_only_iterator DoubleField::begin() const
+{
+	return DoubleField::read_only_iterator(this, 0);
+}
+
+DoubleField::read_only_iterator DoubleField::end() const
+{
+	return DoubleField::read_only_iterator(this, cell_number());
+}
+
+double DoubleField::value(const double x, const double y) const
+{
+	Eigen::Vector2i ij = grid_position(x, y);
+	Eigen::Vector2d pij = position(ij(0), ij(1));
+	Eigen::Vector2d diff(x, y);
+	diff -= pij;
+	double u = diff(0) / _cell_size(0);
+	double v = diff(1) / _cell_size(1);
+	return (1 - u) * (1 - v) * value(ij(0), ij(1)) + (1 - u) * v * value(ij(0), ij(1) + 1) + u * (1 - v) * value(ij(0) + 1, ij(1)) + u * v * value(ij(0) + 1, ij(1) + 1);
+}
+
+double DoubleField::get_min() const
+{
+	return *std::min_element(begin(), end());
+}
+
+double DoubleField::get_max() const
+{
+	return *std::max_element(begin(), end());
+}
+
+double DoubleField::get_range() const
+{
+	double valMin = get_min();
+	double valMax = get_max();
+	return valMax - valMin;
+}
+
+double DoubleField::get_sum() const
+{
+	double sum = 0;
+
+	for(int j = 0; j < grid_height(); ++j)
+	{
+		for(int i = 0; i < grid_width(); i++)
+		{
+			sum += value(i, j);
+		}
+	}
+
+	return sum;
+}
+
+double DoubleField::slope(const Eigen::Vector2i p) const
+{
+	return slope(p(0), p(1));
+}
+double DoubleField::slope(const int i, const int j) const
+{
+	Eigen::Vector2d grad = gradient(i, j);
+	return sqrt(grad.dot(grad));
+}
+
+Eigen::Vector2d DoubleField::gradient(const int i, const int j) const
+{
+	Eigen::Vector2d grad;
+	double delta_x = width() / _grid_width;
+	double delta_y = height() / _grid_height;
+	double scale = 1;
+
+	if(i <= 0)
+	{
+		grad(0) = (value(i + 1, j) - value(i, j)) / delta_x;
+	}
+	else if(i >= _grid_width - 1)
+	{
+		grad(0) = (value(i, j) - value(i - 1, j)) / delta_x;
+	}
+	else
+	{
+		grad(0) = (value(i + 1, j) - value(i - 1, j)) / (2.0 * delta_x);
+	}
+
+	if(j <= 0)
+	{
+		grad(1) = (value(i, j + 1) - value(i, j)) / delta_y;
+	}
+	else if(j >= _grid_height - 1)
+	{
+		grad(1) = (value(i, j) - value(i, j - 1)) / delta_y;
+	}
+	else
+	{
+		grad(1) = (value(i, j + 1) - value(i, j - 1)) / (2.0 * delta_y);
+	}
+
+	return scale * grad;
+}
+
+Eigen::Vector3d DoubleField::normal(const int i, const int j) const
+{
+	Eigen::Vector3d result;
+	Eigen::Vector2d grad = gradient(i, j);
+	result[0] = -grad[0];
+	result[1] = -grad[1];
+	result[2] = 1;
+	result.normalize();
+	return result;
+}
+
+std::vector<std::pair<double, Eigen::Vector2i>> DoubleField::sort_by_height() const
+{
+	std::vector<std::pair<double, Eigen::Vector2i>> sorted_indices = export_to_list();
+	std::sort(sorted_indices.begin(), sorted_indices.end(), [](const std::pair<double, Eigen::Vector2i>& a, const std::pair<double, Eigen::Vector2i>& b)
+	{
+		return a.first > b.first;
+	});
+	return sorted_indices;
+}
+
+int DoubleField::neighbors_info(const int i, const int j, double v[8], Eigen::Vector2i p[8], double s[8]) const
+{
+	int nb = neighbors(i, j, p);
+	double ij_value = value(i, j);
+
+	for(int k = 0; k < nb; ++k)
+	{
+		v[k] = value(p[k]);
+		s[k] = (v[k] - ij_value) / def_nei_dist[k];
+	}
+
+	return nb;
+}
+
+int DoubleField::neighbors_info_filter(const int i, const int j, double v[8], Eigen::Vector2i p[8], double s[8], const double s_filter, const bool sup) const
+{
+	int nb = neighbors(i, j, p);
+	int threshold_nb = 0;
+	double ij_value = value(i, j);
+
+	for(int ineigh = 0; ineigh < nb; ++ineigh)
+	{
+		// values are computed in place but will be overridden / not considered if threshold_nb is not incremented
+		p[threshold_nb] = p[ineigh];
+		v[threshold_nb] = value(p[threshold_nb]);
+		s[threshold_nb] = (v[threshold_nb] - ij_value) / def_nei_dist[ineigh];
+
+		if(sup)
+		{
+			if(s[threshold_nb] >= s_filter)
+			{
+				++threshold_nb;
+			}
+		}
+		else
+		{
+			if(s[threshold_nb] <= s_filter)
+			{
+				++threshold_nb;
+			}
+		}
+	}
+
+	return threshold_nb;
+}
+
+
+DoubleField& DoubleField::operator=(const DoubleField& sf)
+{
+	if(this != &sf)
+	{
+		Grid2d::operator=(sf);
+	}
+
+	return *this;
+}
+
+DoubleField& DoubleField::operator=(DoubleField&& sf)
+{
+	if(this != &sf)
+	{
+		Grid2d::operator=(sf);
+	}
+
+	return *this;
+}
+
+void DoubleField::export_as_obj(const std::string filename, const std::string name) const
+{
+	// Open the output file
+	std::ofstream output(filename, std::ofstream::out);
+
+	// Give the name to the object if specified
+	if(name != "")
+	{
+		output << "o " << name << std::endl;
+	}
+
+	// Set the information for each points
+	for(int j = 0; j < _grid_height; ++j)
+	{
+		// a => the lower left corner of the field in world coordinate
+		// Calculate the y coordinate
+		double vy = _a[1] + j * _cell_size[1];
+
+		for(int i = 0; i < _grid_width; ++i)
+		{
+			// Compute the normal of the point
+			Eigen::Vector3d norm = normal(i, j);
+			// Set the vertex information
+			output << "v " << _a[0] + i * _cell_size[0] << " " << value(i, j) << " " << vy << std::endl;
+			// Set the texture information
+			output << "vt " << (double)i / (_grid_width - 1) << " " << (double)j / (_grid_height - 1) << std::endl;
+			// Set the normal information
+			output << "vn " << norm[0] << " " << norm[2] << " " << norm[1] << std::endl;
+			output << std::endl;
+		}
+	}
+
+	// Set the information for each face
+	for(int j = 0; j < _grid_height - 1; ++j)
+	{
+		for(int i = 1; i < _grid_width; ++i)
+		{
+			int id0 = j * _grid_width + i;
+			int id1 = j * _grid_width + i + 1;
+			int id2 = (j + 1) * _grid_width + i;
+			output << "f " << id0 << "/" << id0 << "/" << id0;
+			output << " " << id1 << "/" << id1 << "/" << id1;
+			output << " " << id2 << "/" << id2 << "/" << id2 << std::endl;
+			id0 = j * _grid_width + i + 1;
+			id1 = (j + 1) * _grid_width + i + 1;
+			id2 = (j + 1) * _grid_width + i;
+			output << "f " << id0 << "/" << id0 << "/" << id0;
+			output << " " << id1 << "/" << id1 << "/" << id1;
+			output << " " << id2 << "/" << id2 << "/" << id2 << std::endl;
+		}
+	}
+
+	output.close();
+}
+
+
+void DoubleField::export_as_pgm(const std::string filename, bool minMax, double rangeMin, double rangeMax) const
+{
+	int maxVal = 255;
+	std::ofstream output(filename, std::ofstream::out);
+	output << "P2" << std::endl;
+	output << _grid_width << " " << _grid_height << std::endl;
+	output << maxVal << std::endl;
+
+	if(minMax)
+	{
+		rangeMax = get_max();
+		rangeMin = get_min();
+	}
+
+	float range = rangeMax - rangeMin;
+
+	for(int j = _grid_height - 1; j >= 0; --j)
+	{
+		for(int i = 0; i < _grid_width; ++i)
+		{
+			output << static_cast<int>(((std::max((std::min(value(i, j), rangeMax) - rangeMin), 0.) / range)*maxVal)) << " ";
+		}
+
+		output << std::endl;
+	}
+}
+
+
+std::vector<std::pair<double, Eigen::Vector2i>> DoubleField::export_to_list() const
+{
+	std::vector<std::pair<double, Eigen::Vector2i>> field;
+
+	for(int j = 0; j < _grid_height; ++j)
+	{
+		for(int i = 0 ; i < _grid_width; ++i)
+		{
+			field.push_back(std::make_pair(value(i, j), Eigen::Vector2i(i, j)));
+		}
+	}
+
+	return field;
+}
+
+double DoubleField::read_only_iterator::operator*() const
+{
+	return _parent->value(_parent->posi_from_index(_position));
+}
+
+DoubleField::read_only_iterator& DoubleField::read_only_iterator::operator++()
+{
+	_position++;
+	return *this;
+}
+
+DoubleField::read_only_iterator DoubleField::read_only_iterator::operator++(int i)
+{
+	read_only_iterator roi = *this;
+	_position++;
+	return roi;
+}
+
+bool DoubleField::read_only_iterator::operator==(const read_only_iterator& roi) const
+{
+	return _parent == roi._parent && _position == roi._position;
+}
+
+bool DoubleField::read_only_iterator::operator!=(const read_only_iterator& roi) const
+{
+	return !(*this == roi);
+}
+
+DoubleField::read_only_iterator& DoubleField::read_only_iterator::operator=(const read_only_iterator& roi)
+{
+	_parent = roi._parent;
+	_position = roi._position;
+	return *this;
+}
